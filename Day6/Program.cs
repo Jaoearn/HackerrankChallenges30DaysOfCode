8de968182eafa7ec86e2f34a3bd30c34695a63e7
jaoearn <jaoearn.sp@gmail.com>
2023-04-17 16:09:59 +0700

    submit code day 6

---
+++ Day6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            static void PrintResult(string word)
+            {
+                string Even = ""; 
+                string Odd = "";
+
+                for (int i = 0; i < word.Length; i++)
+                {
+
+                    if (i % 2 == 0)
+                    {
+                        Even = Even + word[i];
+                    }
+                    else
+                    {
+                        Odd = Odd + word[i];
+                    }
+                }
+                Console.WriteLine("{0} {1}", Even, Odd);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                PrintResult(Console.ReadLine().Trim());
+            }
+        }
+    }
+}
