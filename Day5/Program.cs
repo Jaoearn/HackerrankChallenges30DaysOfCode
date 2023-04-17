054c999ca0260f7b7a4d65a78782afa9f90d583b
jaoearn <jaoearn.sp@gmail.com>
2023-04-17 15:50:55 +0700

    submit code day 5

---
+++ Day5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+            if (n >= 2 && 20 >= n)
+            {
+                for (int i = 1; i < 11; i++)
+                {
+                    string input = n.ToString();
+                    string loop = i.ToString();
+                    int result = n * i;
+                    Console.WriteLine(input + " x " + loop + " = " + result);
+                }
+            }
+        }
+    }
+}
