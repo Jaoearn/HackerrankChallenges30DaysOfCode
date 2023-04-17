ea7779363324dcf21390b4f571d13e4a65fe22f6
jaoearn <jaoearn.sp@gmail.com>
2023-04-17 16:21:16 +0700

    submit code day 7

---
+++ Day7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.Write(arr[n - 1 - i] + " ");
+            }
+        }
+    }
+}
