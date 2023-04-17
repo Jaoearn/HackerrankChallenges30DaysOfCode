e0fcbdb7f164575ca1a75c6dfc81a489f44156ff
jaoearn <jaoearn.sp@gmail.com>
2023-04-17 12:56:53 +0700

    submit code day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine().Trim());
+            if (N % 2 == 0)
+            {
+                if (N <= 5 && N >= 2)
+                {
+                    Console.Write("Not Weird");
+                }
+                else if (N <= 20 && N >= 6)
+                {
+                    Console.Write("Weird");
+                }
+                else
+                {
+                    Console.Write("Not Weird");
+                }
+            }
+            else
+            {
+                Console.Write("Weird");
+            }
+        }
+    }
+}
