9d27fa92b70981928b73e88f50fbaea4820de107
jaoearn <jaoearn.sp@gmail.com>
2023-04-17 15:29:50 +0700

    submit code day 4

---
+++ Day4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Day4
+{
+    class Program
+    {
+        public int age;
+        public Program(int initialAge)
+        {
+            if (initialAge < 0)
+            {
+                Console.WriteLine("Age is not valid, setting age to 0.");
+                this.age = 0;
+            }
+            else
+            {
+                this.age = initialAge;
+            }
+        }
+        public void amIOld()
+        {
+            if (age < 13)
+            {
+                Console.WriteLine("You are young.");
+            }
+            else if (age >= 13 && age < 18)
+            {
+                Console.WriteLine("You are a teenager.");
+            }
+            else
+            {
+                Console.WriteLine("You are old.");
+            }
+        }
+
+        public void yearPasses()
+        {
+            this.age += 1;
+        }
+
+        static void Main(String[] args)
+        {
+            int T = int.Parse(Console.In.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                int age = int.Parse(Console.In.ReadLine());
+                Program p = new Program(age);
+                p.amIOld();
+                for (int j = 0; j < 3; j++)
+                {
+                    p.yearPasses();
+                }
+                p.amIOld();
+                Console.WriteLine();
+            }
+        }
+    }
+}
