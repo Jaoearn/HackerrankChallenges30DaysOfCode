04d16578c17b05c4216804f5577a7dcbcc5a4491
jaoearn <jaoearn.sp@gmail.com>
2023-04-18 10:55:38 +0700

    submit code day 8

---
+++ Day8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, string> phoneNumber = new Dictionary<string, string>();
+            for (int i = 0; i < n; i++)
+            {
+                string[] seperatedInput = Console.ReadLine().Split(' ');
+                phoneNumber.Add(seperatedInput[0], seperatedInput[1]);
+            }
+            string input;
+            while ((input = Console.ReadLine()) != null)
+            {
+                if (phoneNumber.ContainsKey(input))
+                {
+                    Console.WriteLine($"{input}={phoneNumber[input]}");
+                }
+                else
+                {
+                    Console.WriteLine("Not found");
+                }
+            }
+        }
+    }
+}
