741607ae563efc2549754b73d8087a4d22b1b9c0
jaoearn <jaoearn.sp@gmail.com>
2023-04-20 16:20:15 +0700

    submit code c# day 9

--- C#/Day9/Program.cs
+++ C#/Day9/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.IO;
 
 namespace Day9
 {
+    class Result
+    {
+        public static int factorial(int n)
+        {
+
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * factorial(n - 1);
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            int result = Result.factorial(n);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
         }
     }
 }
