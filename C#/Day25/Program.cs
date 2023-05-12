df3b2c81bd4bfd2eda985a43e9c8c21b11ba470b
jaoearn <jaoearn.sp@gmail.com>
2023-05-12 18:04:14 +0700

    submit code c# day 25

---
+++ C#/Day25/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Day25
+{
+    class Program
+    {
+        public static bool isPrime(int number)
+        {
+            if (number <= 1)
+                return false;
+
+            for (int divisor = 2; divisor <= Math.Sqrt(number); divisor++)
+                if (number % divisor == 0)
+                    return false;
+            return true;
+        }
+        static void Main(String[] args)
+        {
+            int times = int.Parse(Console.ReadLine());
+            while (times-- > 0)
+            {
+                int input = int.Parse(Console.ReadLine());
+                Console.WriteLine(isPrime(input) ? "Prime" : "Not prime");
+            }
+        }
+    }
+}
