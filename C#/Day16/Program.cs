4c7e1fc06770e30dfaf9f237e5d34307112d7670
jaoearn <jaoearn.sp@gmail.com>
2023-05-12 14:31:49 +0700

    submit code c# day 16

---
+++ C#/Day16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Day16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string S = Console.ReadLine();
+            try
+            {
+                var result = Convert.ToInt32(S);
+                Console.WriteLine(result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Bad String");
+            }
+        }
+    }
+}
